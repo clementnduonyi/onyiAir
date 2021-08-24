6dd3761e3f49703ae8854b7a9308d5a33a1b3378
Cendevs <cndigitech@gmail.com>
2021-08-25 00:34:03 +0100

    add  pg extension

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2021_08_03_210333) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "airports", force: :cascade do |t|
     t.string "code"
     t.string "town"
